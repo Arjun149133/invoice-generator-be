84eb4bcfa157c90701f09ed19117db1fbbc86f8e
Arjun Yadav <arjun149133@gmail.com>
2025-08-21 12:56:57 +0000

    pdf-format

--- api/services/utils.ts
+++ api/services/utils.ts
@@ -1,52 +1,71 @@
 import prisma from "../config/db";
-import puppeteer from "puppeteer";
+import PuppeteerHTMLPDF from "puppeteer-html-pdf";
+import hbs from "handlebars";
+import path from "path";
+import fs from "fs";
 
-const generatePdf = async (invoiceId: string) => {
-  const invoice = await prisma.invoice.findUnique({
-    where: {
-      id: invoiceId,
-    },
-    include: {
-      products: true,
-      user: true,
-    },
-  });
+export const generatePdf = async (invoiceId: string) => {
+  const htmlPDF = new PuppeteerHTMLPDF();
+  htmlPDF.setOptions({ format: "A4" });
 
-  if (!invoice) {
-    throw new Error("Invoice not found");
+  hbs.registerHelper("multiply", (a, b) => a * b);
+  const invoiceData = await getInvoiceData(invoiceId);
+
+  const pdfData = {
+    date: "21/08/25",
+    user: {
+      username: invoiceData?.user?.username || "John Doe",
+      email: invoiceData?.user?.email || "john@example.com",
+    },
+    products: invoiceData.products.map((product) => ({
+      name: product.name,
+      qty: product.quantity,
+      rate: product.rate,
+      price: product.quantity * product.rate,
+    })),
+    totalAmount: invoiceData.totalAmount || 22000, // e.g., 22000 cents
+    gst: invoiceData.gst, // e.g., 18% of 22000 in cents
+    gstRounded: Math.round(invoiceData.gst / 100), // e.g., 3960 cents
+    totalPayable: invoiceData.totalAmount + Math.round(invoiceData.gst / 100),
+  };
+
+  try {
+    const cssPath = path.join(__dirname, "../../pdf-format/css/main.css");
+    const cssRaw = await fs.promises.readFile(cssPath, "utf8");
+    const cssContent = cssRaw.replace(/^\uFEFF/, ""); // Removes BOM if exists
+
+    const html = await htmlPDF.readFile(
+      path.join(__dirname, "../../pdf-format/index.html"),
+      "utf8"
+    );
+    const template = hbs.compile(html);
+    const content = template({ ...pdfData, styles: cssContent });
+
+    console.log("Generated HTML content for PDF:", content);
+    const pdfBuffer = await htmlPDF.create(content);
+
+    return pdfBuffer;
+  } catch (error) {
+    console.error("PuppeteerHTMLPDF error", error);
+
+    throw error;
   }
-
-  const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-  });
-  const page = await browser.newPage();
-
-  const html = `
-    <html>
-      <head><style>body{font-family:sans-serif;}</style></head>
-      <body>
-        <h1>Invoice</h1>
-        <p>Customer: ${invoice.user.username}</p>
-        <ul>
-          ${invoice.products
-            .map((p: any) => `<li>${p.name} - $${p.price}</li>`)
-            .join("")}
-        </ul>
-        <h2>TotalCharges: $${invoice.totalAmount}</h2>
-        <h2>GST: $${Math.round(invoice.gst / 100)}</h2>
-
-        <h3>TotalPayable: $${
-          invoice.totalAmount + Math.round(invoice.gst / 100)
-        }</h3>
-      </body>
-    </html>
-  `;
-
-  await page.setContent(html);
-  const pdfBuffer = await page.pdf({ format: "A4" });
-
-  await browser.close();
-  return pdfBuffer;
 };
 
-export { generatePdf };
+const getInvoiceData = async (invoiceId: string) => {
+  try {
+    const invoice = await prisma.invoice.findUnique({
+      where: {
+        id: invoiceId,
+      },
+      include: {
+        products: true,
+        user: true,
+      },
+    });
+    return invoice;
+  } catch (error) {
+    console.error("Error fetching invoice data:", error);
+    throw error;
+  }
+};
