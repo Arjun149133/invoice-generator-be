a6fb09681bbe20948ccd5cbaab13b97e6b49fd84
Arjun Yadav <arjun149133@gmail.com>
2025-08-22 03:50:39 +0000

    vercel deploy

--- api/middlewares/authMiddleware.ts
+++ api/middlewares/authMiddleware.ts
@@ -10,7 +10,7 @@ const authMiddleware = async (
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) throw new Error("No token provided");
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
 
     if (decoded && typeof decoded === "object" && "userId" in decoded) {
       req.userId = decoded.userId;
